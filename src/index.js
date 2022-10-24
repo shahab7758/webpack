78976a512461783b631cd3e0fce018e1259492cd
shahab Ramzan <96526562+shahab-ramzan@users.noreply.github.com>
2022-10-25 01:14:59 +0500

    image loader config added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
  import _ from 'lodash';
 import './style.css';
+import Pet from './pet.jpeg'
 
  function component() {
    const element = document.createElement('div');
@@ -7,6 +8,12 @@ import './style.css';
    // Lodash, now imported by this script
    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
+
+  // add image 
+
+  const myPet = new Image()
+  myPet.src = Pet
+  element.appendChild(myPet)
 
    return element;
  }
